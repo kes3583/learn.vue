1ad4060a091be3ddb4eda9be3a6f4e0ea77a2195
kes3583 <kes3583@gmail.com>
2018-09-21 10:05:19 +0200

    to done lists

--- part11/index.js
+++ part11/index.js
@@ -1,30 +1,42 @@
 const title = "practical component Exercise 1 message",
-selectTitle = document.querySelector('title');
+  selectTitle = document.querySelector('title');
 selectTitle.innerHTML = title;
 
 
-Vue.component('modal', {
+Vue.component('tabs', {
   //props: ['msg'],
   template: `
-  <div class="modal is-active">
-      <div class="modal-background"></div>
-      <div class="modal-content">
-          <div class="box">
-            <slot></slot>
-          </div>
-      </div>
-      <button @click="$emit('close')" class="modal-close is-large" aria-label="close"></button>
+  <div class="tabs">
+    <ul>
+      <li class="is-active"><a>Pictures</a></li>
+      <li><a href="#tab1" id="tab1">Music</a></li>
+      <li><a href="#tab2" id="tab2">Videos</a></li>
+      <li><a href="#tab3" id="tab3">Documents</a></li>
+    </ul>
   </div>
   `,
-  data(){
-      return {}
+  data() {
+    return {}
   }
 })
-
+Vue.component('tab', {
+  //props: ['msg'],
+  template: `
+    <li><a href="#">Pictures</a></li>
+  `
+})
+Vue.component('tab-panel', {
+  //props: ['msg'],
+  template: `
+    <div class="tab-panels" id="tab1">
+      <p>tab1</p>
+    </div>
+  `
+})
 
 new Vue({
   el: '#root',
-  data:{
-    showModal : false
+  data: {
+    showModal: false
   }
 })
